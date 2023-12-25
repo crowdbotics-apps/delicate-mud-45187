fb3b68ea8d38af3bcd338cf4dfd53e401a7db6d4
Crowdbotics <team@crowdbotics.com>
2023-12-25 06:58:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-mud-45187",
+  url: "https://delicate-mud-45187.botics.co",
+  api: "https://delicate-mud-45187.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
